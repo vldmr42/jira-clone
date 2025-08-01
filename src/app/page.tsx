537539773ddf4ce57977bd75c4f7ec8d07d8063d
vldmr42 <kmb14056@yandex.ru>
2025-08-01 12:16:58 +0400

    buttons setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,14 @@ import { Button } from '@/components/ui/button';
 
 export default function Home() {
     return (
-        <div>
-            <Button>Click Me</Button>
+        <div className="flex gap-6">
+            <Button>Primary</Button>
+            <Button variant={'secondary'}>Secondary</Button>
+            <Button variant={'ghost'}>Ghost</Button>
+            <Button variant={'outline'}>Outline</Button>
+            <Button variant={'destructive'}>Destructive</Button>
+            <Button variant={'muted'}>Muted</Button>
+            <Button variant={'tertiary'}>Tertiary</Button>
         </div>
     );
 }
