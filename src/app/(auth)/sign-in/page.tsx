b633d746fae9e1e2215fa3d95ff4b4d96f4615eb
vldmr42 <kmb14056@yandex.ru>
2025-08-01 15:48:47 +0400

    custom dotted separator component

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,5 +1,7 @@
+import { SignInCard } from '@/features/auth/components/sign-in-card';
+
 const SignInPage = () => {
-    return <div>Sign in page</div>;
+    return <SignInCard />;
 };
 
 export default SignInPage;
