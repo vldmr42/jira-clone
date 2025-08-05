c3687f4a3c771efa16814e0ba6dc7109e7864aa5
vldmr42 <kmb14056@yandex.ru>
2025-08-05 20:19:57 +0400

    logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,14 @@
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
+import { Button } from '@/components/ui/button';
 import { useCurrent } from '@/features/auth/api/use-current';
+import { useLogout } from '@/features/auth/api/use-logout';
 
 export default function Home() {
     const router = useRouter();
     const { data, isLoading } = useCurrent();
+    const { mutate } = useLogout();
 
     useEffect(() => {
         if (!data && !isLoading) {
@@ -15,5 +18,10 @@ export default function Home() {
         }
     }, [data]);
 
-    return <div className="flex gap-6">Only visible to authorized users</div>;
+    return (
+        <div className="flex gap-6">
+            Only visible to authorized users
+            <Button onClick={() => mutate()}>Logout</Button>
+        </div>
+    );
 }
