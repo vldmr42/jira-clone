3158f3bcbcbce51b333b5ec90129004524110927
vldmr42 <kmb14056@yandex.ru>
2025-08-17 17:53:57 +0400

    fix workspace-switcher-outline

--- src/features/workspaces/components/workspace-avatar.tsx
+++ src/features/workspaces/components/workspace-avatar.tsx
@@ -28,7 +28,7 @@ export const WorkspaceAvatar = ({
     }
 
     return (
-        <Avatar className={cn('size-10', className)}>
+        <Avatar className={cn('size-10 rounded-md', className)}>
             <AvatarFallback className="text-white bg-blue-600 font-semibold text-lg uppercase">
                 {name[0]}
             </AvatarFallback>
