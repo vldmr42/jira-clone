26cb5c78a62600bfec13d72846e96b3385d1651d
vldmr42 <kmb14056@yandex.ru>
2025-08-17 17:46:21 +0400

    fix image error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['fra.cloud.appwrite.io'],
+    },
+};
 
 export default nextConfig;
