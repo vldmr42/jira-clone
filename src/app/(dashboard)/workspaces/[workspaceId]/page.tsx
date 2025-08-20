0a3d571555af42f955d3dbb8d43c94ee5851b525
vldmr42 <kmb14056@yandex.ru>
2025-08-20 09:50:35 +0400

    sign-in redirection

--- src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -1,4 +1,13 @@
-const WorkspaceIdPage = () => {
+import { redirect } from 'next/navigation';
+
+import { getCurrent } from '@/features/auth/actions';
+
+const WorkspaceIdPage = async () => {
+    const user = await getCurrent();
+    if (!user) {
+        redirect('/sign-in');
+    }
+
     return <div>Workspace Id</div>;
 };
 
--- src/app/(standalone)/workspaces/create/page.tsx
+++ src/app/(standalone)/workspaces/create/page.tsx
@@ -1,6 +1,14 @@
+import { redirect } from 'next/navigation';
+
+import { getCurrent } from '@/features/auth/actions';
 import { CreateWorkspaceForm } from '@/features/workspaces/components/create-workspace-form';
 
-const WorkspaceCreatePage = () => {
+const WorkspaceCreatePage = async () => {
+    const user = await getCurrent();
+    if (!user) {
+        redirect('/sign-in');
+    }
+
     return (
         <div className="w-full lg:max-w-xl">
             <CreateWorkspaceForm />
