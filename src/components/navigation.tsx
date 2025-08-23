ad142abb81476d105d99e97c3473421e77afc872
vldmr42 <kmb14056@yandex.ru>
2025-08-23 16:47:22 +0400

    navigation links fix

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import { SettingsIcon, UsersIcon } from 'lucide-react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import {
     GoCheckCircle,
     GoCheckCircleFill,
@@ -7,6 +10,7 @@ import {
     GoHomeFill,
 } from 'react-icons/go';
 
+import { useWorkspaceId } from '@/features/workspaces/hooks/use-workspace-id';
 import { cn } from '@/lib/utils';
 
 const routes = [
@@ -37,13 +41,17 @@ const routes = [
 ];
 
 export const Navigation = () => {
+    const workspaceId = useWorkspaceId();
+    const pathname = usePathname();
+
     return (
         <ul className="flex flex-col">
             {routes.map((item) => {
-                const isActive = false;
+                const fullHref = `/workspaces/${workspaceId}${item.href}`;
+                const isActive = pathname === fullHref;
                 const Icon = isActive ? item.activeIcon : item.icon;
                 return (
-                    <Link key={item.href} href={item.href}>
+                    <Link key={item.href} href={fullHref}>
                         <div
                             className={cn(
                                 'flex items-center gap-2.5 p-2.5 rounded-md font-medium hover:text-primary transition text-neutral-500',
