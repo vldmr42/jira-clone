96699281e23502ae0ae028a9f2b2234d9c30bde3
vldmr42 <kmb14056@yandex.ru>
2025-09-07 10:01:06 +0400

    hooks for members api

---
+++ src/features/members/api/use-get-members.ts
@@ -0,0 +1,29 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { client } from '@/lib/rpc';
+
+interface UseGetMembersProps {
+    workspaceId: string;
+}
+
+export const useGetMembers = ({ workspaceId }: UseGetMembersProps) => {
+    const query = useQuery({
+        queryKey: ['members', workspaceId],
+        queryFn: async () => {
+            const response = await client.api.members.$get({
+                query: { workspaceId },
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to fetch workspaces');
+                // return { total: 0, documents: [] };
+            }
+
+            const { data } = await response.json();
+
+            return data;
+        },
+    });
+
+    return query;
+};
