8ee5d6c64d40a42cb96f99482c99994b1bb63017
vldmr42 <kmb14056@yandex.ru>
2025-09-11 22:23:52 +0400

    projects create api

--- src/features/projects/server/route.ts
+++ src/features/projects/server/route.ts
@@ -1,40 +1,102 @@
 import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
-import { Query } from 'node-appwrite';
+import { ID, Query } from 'node-appwrite';
 import z from 'zod';
 
-import { DATABASE_ID, PROJECTS_COLLECTION_ID } from '@/config';
+import {
+    DATABASE_ID,
+    IMAGES_BUCKET_ID,
+    PROJECTS_COLLECTION_ID,
+} from '@/config';
 import { getMember } from '@/features/members/utils';
 import { sessionMiddleware } from '@/lib/session-middleware';
 
-const app = new Hono().get(
-    '/',
-    sessionMiddleware,
-    zValidator('query', z.object({ workspaceId: z.string() })),
-    async (c) => {
-        const user = c.get('user');
-        const databases = c.get('databases');
+import { createProjectSchema } from '../schemas';
 
-        const { workspaceId } = c.req.valid('query');
+const app = new Hono()
+    .get(
+        '/',
+        sessionMiddleware,
+        zValidator('query', z.object({ workspaceId: z.string() })),
+        async (c) => {
+            const user = c.get('user');
+            const databases = c.get('databases');
 
-        const member = await getMember({
-            databases,
-            workspaceId,
-            userId: user.$id,
-        });
+            const { workspaceId } = c.req.valid('query');
 
-        if (!member) {
-            return c.json({ error: 'Unauthorized' }, 401);
+            const member = await getMember({
+                databases,
+                workspaceId,
+                userId: user.$id,
+            });
+
+            if (!member) {
+                return c.json({ error: 'Unauthorized' }, 401);
+            }
+
+            const projects = await databases.listDocuments(
+                DATABASE_ID,
+                PROJECTS_COLLECTION_ID,
+                [Query.equal('workspaceId', workspaceId)]
+            );
+
+            return c.json({ data: projects });
         }
+    )
+    .post(
+        '/',
+        sessionMiddleware,
+        zValidator('form', createProjectSchema),
+        async (c) => {
+            const databases = c.get('databases');
+            const storage = c.get('storage');
+            const user = c.get('user');
 
-        const projects = await databases.listDocuments(
-            DATABASE_ID,
-            PROJECTS_COLLECTION_ID,
-            [Query.equal('workspaceId', workspaceId)]
-        );
+            const { name, image, workspaceId } = c.req.valid('form');
 
-        return c.json({ data: projects });
-    }
-);
+            const member = await getMember({
+                databases,
+                workspaceId,
+                userId: user.$id,
+            });
+
+            if (!member) {
+                return c.json({ error: 'Unauthorized' });
+            }
+
+            let uploadedImageUrl: string | undefined;
+
+            if (image instanceof File) {
+                const file = await storage.createFile(
+                    IMAGES_BUCKET_ID,
+                    ID.unique(),
+                    image
+                );
+
+                const arrayBuffer = await storage.getFileView(
+                    IMAGES_BUCKET_ID,
+                    file.$id
+                );
+
+                uploadedImageUrl = `data:image/png;base64,${Buffer.from(
+                    arrayBuffer
+                ).toString('base64')}`;
+            }
+
+            const project = await databases.createDocument(
+                DATABASE_ID,
+                PROJECTS_COLLECTION_ID,
+                ID.unique(),
+                {
+                    name,
+                    userId: user.$id,
+                    imageUrl: uploadedImageUrl,
+                    workspaceId,
+                }
+            );
+
+            return c.json({ data: project });
+        }
+    );
 
 export default app;
