c769a2637d9c4bb2972afd37e3f45d896d57cf9b
vldmr42 <kmb14056@yandex.ru>
2025-10-12 15:53:55 +0400

    tasks schema and tasks-switcher component

--- src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
@@ -6,6 +6,7 @@ import { Button } from '@/components/ui/button';
 import { getCurrent } from '@/features/auth/queries';
 import { ProjectAvatar } from '@/features/projects/components/project-avatar';
 import { getProject } from '@/features/projects/queries';
+import { TaskViewSwitcher } from '@/features/tasks/components/task-view-switcher';
 
 interface ProjectIdPageProps {
     params: { projectId: string };
@@ -50,6 +51,7 @@ const ProjectIdPage = async ({ params }: ProjectIdPageProps) => {
                     </Button>
                 </div>
             </div>
+            <TaskViewSwitcher />
         </div>
     );
 };
---
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -0,0 +1,54 @@
+import { PlusIcon } from 'lucide-react';
+
+import { DottedSeparator } from '@/components/dotted-separator';
+import { Button } from '@/components/ui/button';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
+export const TaskViewSwitcher = () => {
+    return (
+        <Tabs className="flex-1 w-full border rounded-lg">
+            <div className="h-full flex flex-col overflow-auto p-4">
+                <div className="flex flex-col gap-y-2 lg:flex-row justify-between items-center">
+                    <TabsList className="w-full lg:w-auto">
+                        <TabsTrigger
+                            className="h-8 w-full lg:w-auto"
+                            value="table"
+                        >
+                            Table
+                        </TabsTrigger>
+                        <TabsTrigger
+                            className="h-8 w-full lg:w-auto"
+                            value="kanban"
+                        >
+                            Kanban
+                        </TabsTrigger>
+                        <TabsTrigger
+                            className="h-8 w-full lg:w-auto"
+                            value="calendar"
+                        >
+                            Calendar
+                        </TabsTrigger>
+                    </TabsList>
+                    <Button size={'sm'} className="w-full lg:w-auto">
+                        <PlusIcon className="size-4 mr-4" />
+                        New
+                    </Button>
+                </div>
+                <DottedSeparator className="my-4" />
+                {/* Add filters */}
+                <DottedSeparator className="my-4" />
+                <>
+                    <TabsContent value="table" className="mt-0">
+                        Data Table
+                    </TabsContent>
+                    <TabsContent value="kanban" className="mt-0">
+                        Data Kanban
+                    </TabsContent>
+                    <TabsContent value="calendar" className="mt-0">
+                        Data Calendar
+                    </TabsContent>
+                </>
+            </div>
+        </Tabs>
+    );
+};
