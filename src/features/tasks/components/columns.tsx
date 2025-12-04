23bdd0eaa35cb714c70a753bb2702982b1f2a2f4
vldmr42 <kmb14056@yandex.ru>
2025-12-04 21:10:37 +0400

    data-table sorting

--- src/features/tasks/components/columns.tsx
+++ src/features/tasks/components/columns.tsx
@@ -1,12 +1,25 @@
 'use client';
 
 import { ColumnDef } from '@tanstack/react-table';
+import { ArrowUpDown } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
 
 import { Task } from '../types';
 
 export const columns: ColumnDef<Task>[] = [
   {
     accessorKey: 'name',
-    header: 'Task Name',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Task Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
   },
 ];
--- src/features/tasks/components/data-table.tsx
+++ src/features/tasks/components/data-table.tsx
@@ -5,8 +5,11 @@ import {
   flexRender,
   getCoreRowModel,
   getPaginationRowModel,
+  getSortedRowModel,
+  SortingState,
   useReactTable,
 } from '@tanstack/react-table';
+import * as React from 'react';
 
 import { Button } from '@/components/ui/button';
 import {
@@ -27,11 +30,17 @@ export function DataTable<TData, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+    },
   });
 
   return (
