be98fbd4981049cff6601a44cb09993083e29950
vldmr42 <kmb14056@yandex.ru>
2026-02-06 20:22:57 +0400

    drag and drop start

--- src/features/tasks/components/data-kanban.tsx
+++ src/features/tasks/components/data-kanban.tsx
@@ -1,4 +1,4 @@
-import { DragDropContext } from '@hello-pangea/dnd';
+import { DragDropContext, Draggable, Droppable } from '@hello-pangea/dnd';
 import { useState } from 'react';
 
 import { Task, TaskStatus } from '../types';
@@ -55,6 +55,33 @@ export const DataKanban = ({ data }: DataKanbanProps) => {
                 board={board}
                 taskCount={tasks[board].length}
               />
+              <Droppable droppableId={board}>
+                {(provided) => (
+                  <div
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    className="min-h-[200px] py-1.5"
+                  >
+                    {tasks[board].map((task, index) => (
+                      <Draggable
+                        index={index}
+                        key={task.$id}
+                        draggableId={task.$id}
+                      >
+                        {(provided) => (
+                          <div
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            ref={provided.innerRef}
+                          >
+                            {task.name}
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                  </div>
+                )}
+              </Droppable>
             </div>
           );
         })}
--- src/features/tasks/components/kanban-column-header.tsx
+++ src/features/tasks/components/kanban-column-header.tsx
@@ -1,5 +1,17 @@
+import {
+  CircleCheckIcon,
+  CircleDashedIcon,
+  CircleDotDashedIcon,
+  CircleDotIcon,
+  CircleIcon,
+  PlusIcon,
+} from 'lucide-react';
+import React from 'react';
+
+import { Button } from '@/components/ui/button';
 import { snakeCaseToTitleCase } from '@/lib/utils';
 
+import { useCreateTaskModal } from '../hooks/use-create-task-modal';
 import { TaskStatus } from '../types';
 
 interface KanbanColumnHeaderProps {
@@ -7,16 +19,41 @@ interface KanbanColumnHeaderProps {
   taskCount: number;
 }
 
+const statusIconMap: Record<TaskStatus, React.ReactNode> = {
+  [TaskStatus.BACKLOG]: (
+    <CircleDashedIcon className="size-[18px] text-pink-400" />
+  ),
+  [TaskStatus.TODO]: <CircleIcon className="size-[18px] text-red-400" />,
+  [TaskStatus.IN_PROGRESS]: (
+    <CircleDotDashedIcon className="size-[18px] text-yellow-400" />
+  ),
+  [TaskStatus.IN_REVIEW]: (
+    <CircleDotIcon className="size-[18px] text-blue-400" />
+  ),
+  [TaskStatus.DONE]: (
+    <CircleCheckIcon className="size-[18px] text-emerald-400" />
+  ),
+};
+
 export const KanbanColumnHeader = ({
   board,
   taskCount,
 }: KanbanColumnHeaderProps) => {
+  const { open } = useCreateTaskModal();
+
+  const icon = statusIconMap[board];
   return (
     <div className="px-2 py-1.5 flex items-center justify-between">
       <div className="flex items-center gap-x-2">
+        {icon}
         <h2>{snakeCaseToTitleCase(board)}</h2>
+        <div className="size-5 flex items-center justify-center rounded-md bg-neutral-100">
+          {taskCount}
+        </div>
       </div>
-      <div>{taskCount}</div>
+      <Button onClick={open} variant={'ghost'} size="icon" className="size-5">
+        <PlusIcon className="size-4 text-neutral-500" />
+      </Button>
     </div>
   );
 };
